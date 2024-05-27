d4cbd46620e11864d690093fca82128c744671b9
Lucca Dittrich <ldslucca@hotmail.com>
2024-05-27 19:28:58 -0300

    init corrections

--- User.java
+++ User.java
@@ -1,32 +1,36 @@
 package javamysqlapp;
 
-public final class User {   
-    private String  sUser;
-    private String  sPass;
-    
-    public  User()
-    {   clearObject();
+public final class User {
+    private String sUser;
+    private String sPass;
+
+    public User() {
+        clearObject();
     }
-    public  User(String sI, String sU, String sP)
-    {   sUser = sU;
+
+    public User(String sI, String sU, String sP) {
+        sUser = sU;
         sPass = sP;
     }
-    
-    public  String  getUser()
-    {   return  sUser;
+
+    public String getUser() {
+        return sUser;
     }
-    public  void    setUser(String sU)
-    {   sUser = sU;
+
+    public void setUser(String sU) {
+        sUser = sU;
     }
-    
-    public  String  getPass()
-    {   return  sPass;
+
+    public String getPass() {
+        return sPass;
     }
-    public  void    setPass(String sP)
-    {   sPass = sP;
+
+    public void setPass(String sP) {
+        sPass = sP;
     }
-    public  void    clearObject()
-    {   sUser = "Não há usuário cadastrado";
+
+    public void clearObject() {
+        sUser = "Não há usuário cadastrado";
         sPass = "Não há senha cadastrada";
     }
 }
